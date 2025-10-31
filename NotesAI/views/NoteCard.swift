497a7dd7b40f43b7eb9d55b1c92369f5b9e05dfd
Sofia Guerra <sofiaguerraquintero@gmail.com>
2025-10-31 01:08:46 -0400

    same commit

--- NotesAI/views/NoteCard.swift
+++ NotesAI/views/NoteCard.swift
@@ -12,44 +12,46 @@ struct NoteCard: View {
     var note: Note
     
     var body: some View {
-        HStack(spacing: 16) {
+        
+            HStack(spacing: 16) {
                     // Marco vacío (para futura imagen)
                     //Image()
-                    Rectangle()
-                        .strokeBorder(Color.white.opacity(0.3), lineWidth: 2)
-                        .frame(width: 70, height: 70)
-                        .cornerRadius(10)
-                    
+                Rectangle()
+                    .strokeBorder(Color.white.opacity(0.3), lineWidth: 2)
+                    .frame(width: 70, height: 70)
+                    .cornerRadius(10)
+                
                     // Texto temporal (mock data)
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text(note.title) //tiulo de la nota
-                            .font(.headline)
-                            .foregroundColor(.white)
-                        Text(note.description) //descripcion de la nota
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                            .lineLimit(2)
-                    }
-                 
-                    Button{
-                        
-                    }label: {
-                        Image(systemName: "ellipsis")
-                    .foregroundColor(.white.opacity(0.8))
-                                  .font(.title2)
-                                  .padding(.trailing, 4)
-                                  .rotationEffect(.degrees(90))
-                    }
-                    
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(note.title) //tiulo de la nota
+                        .font(.headline)
+                        .foregroundColor(.white)
+                    Text(note.description) //descripcion de la nota
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                        .lineLimit(2)
                 }
-                .padding()
-                .background(Color(red: 0.133, green: 0.133, blue: 0.149))
-
-                // gris azulado oscuro
-                .cornerRadius(12)
-                .shadow(radius: 2)
+                
+                Button{
+                    
+                }label: {
+                    Image(systemName: "ellipsis")
+                        .foregroundColor(.white.opacity(0.8))
+                        .font(.title2)
+                        .padding(.trailing, 4)
+                        .rotationEffect(.degrees(90))
+                }
+                
+            }
+            .padding()
+            .background(Color(red: 0.133, green: 0.133, blue: 0.149))
             
-    }
+                // gris azulado oscuro
+            .cornerRadius(12)
+            .shadow(radius: 2)
+            
+        }
+    
 }
 
 #Preview {
